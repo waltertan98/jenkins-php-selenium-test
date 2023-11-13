16b26e3a71e59c2b1a231dffcd3599a1b17b7c5d
waltertan98 <waltertan99@hotmail.com>
2023-11-13 16:30:24 +0800

    qwe

--- jenkins/scripts/deploy.sh
+++ jenkins/scripts/deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 set -x
-docker run -d -p 80:80 --name my-apache-php-app -v /home/waltertan/jenkins-php-selenium-test/src:/var/www/html php:7.2-apache
+docker run -d -p 8000:8000 --name my-apache-php-app -v /home/waltertan/jenkins-php-selenium-test/src:/var/www/html php:7.2-apache
 sleep 1
 set +x
 
