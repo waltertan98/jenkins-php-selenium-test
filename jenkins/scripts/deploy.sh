425a809f1ebc82ebf95c34b549808156e571ab52
waltertan98 <waltertan99@hotmail.com>
2023-11-13 14:16:05 +0800

    affdgd

--- jenkins/scripts/deploy.sh
+++ jenkins/scripts/deploy.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/user/bin/env sh
 
 set -x
 docker run -d -p 80:80 --name my-apache-php-app -v /home/waltertan/jenkins-php-selenium-test/src:/var/www/html php:7.2-apache
@@ -6,5 +6,5 @@ sleep 1
 set +x
 
 echo 'Now...'
-echo 'Visit http://192.168.128.76 to see your PHP application in action.'
+echo 'Visit http://192.168.0.221 to see your PHP application in action.'
 
--- jenkins/scripts/kill.sh
+++ jenkins/scripts/kill.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/user/bin/env sh
 
 set -x
 docker kill my-apache-php-app
