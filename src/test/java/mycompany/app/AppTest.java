ef71341c025cb1307121315c5558b663a20842f8
waltertan98 <waltertan99@hotmail.com>
2023-11-13 13:45:54 +0800

    asdsad

--- src/test/java/mycompany/app/AppTest.java
+++ src/test/java/mycompany/app/AppTest.java
@@ -23,7 +23,7 @@ public class AppTest
 {
 	WebDriver driver; 
 	WebDriverWait wait; 
-	String url = "http://192.168.128.76";
+	String url = "http://192.168.0.221";
 	String validEmail = "user@example.com";
 	String validPassword = "password1234";
 	String invalidEmail = "none@example.com";
